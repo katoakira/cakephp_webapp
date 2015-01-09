47e040e371a40d5d83cb1c4823f47b908ba78a85
vagrant <vagrant@localhost.localdomain>
2015-01-09 04:35:37 +0000

    First cakephp

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '186d1a6cee989f4ef7062d6554f79d1cf82f2fa4');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '602708278178114427726456871880');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
